8a3b02762c3b254d07a566b39c623c2deb372a1e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-01-23 19:52:32 +0600

    default margin guide

---
+++ DefaultMarginGuide/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  DefaultMarginGuide
+//
+//  Created by Shah Md Imran Hossain on 23/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews() {
+        let redView = UIView()
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            redView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            redView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            redView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+}
+
